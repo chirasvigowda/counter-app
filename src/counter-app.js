15344fdd391149e96635ad18980733a31fc86a76
chirasvigowda <czg5634@psu.edu>
2024-10-09 21:36:07 -0400

    partially complete

--- src/counter-app.js
+++ src/counter-app.js
@@ -10,11 +10,17 @@ export class counterApp extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.title = "";
+    this.counter = 0; 
+    this.minNumber = 0; 
+    this.maxNumber = 30; 
   }
 
   static get properties() {
     return {
       title: { type: String },
+      counter: { type: Number, reflect: true }, 
+      minNumber: { type: Number }, 
+      maxNumber: { type: Number }
     };
   }
 
@@ -31,29 +37,78 @@ export class counterApp extends DDDSuper(LitElement) {
       .wrapper {
         margin: var(--ddd-spacing-2);
         padding: var(--ddd-spacing-4);
+        text-align: center; 
       }
-      div {
-        padding: 0;
-        margin: 0;
+      .counter{
+        font-size: var(--ddd-font-size-s)
+      }
+      :host([counter="18"]) .counter {
+        color: var(--ddd-theme-default-#f03fc4); 
+      }
+      :host([counter="21"]) .counter {
+        color: var(--ddd-theme-default-#1ed1e9); 
+      }
+      :host([counter="0"]) .counter {
+        color: var(--ddd-theme-default-#007f20); 
+      }
+      :host([counter="30"]) .counter {
+        color: var(--ddd-theme-default-#e37611); 
+      }
+      button {
+        font-size: var(--ddd-font-size-s); 
+        background-color: var(--ddd-theme-default-#e5cb6d);
+        padding: var(--ddd-spacing-3); 
+      }
+      button:hover {
+        background-color: var(--ddd-theme-default-#725c5c);
+      }
+      button:disabled {
+        background-color: var(--ddd-theme-default-#747474);
       }
     `];
+  }
+  decrement() {
+    if (this.counter > this.minNumber) {
+      this.counter--; 
+    }
+  }
+  increment() {
+    if (this.counter < this.maxNumber) {
+      this.counter++; 
+    }
+  }
+  updated(changedProperties) {
+    if (changedroperties.has('counter')) {
+      if (this.counter === this.maxNumber) {
+        this.makeItRain();
+      }
+    }
+  }
+  makeItRain() {
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(() => {
+      setTimeout(() => {
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+
+        }, 0);
+      }
+    );
   }
 
   render() {
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
+      <div class="wrapper">
+        <div>${this.title}</div>
+        <div class = "counter">${this.counter}</div>
+        <button @click = "${this.decrement}" ?disabled = "${this.counter === this.maxNumber}">-</button>
+        <button @click = "${this.increment}" ?disabled = "${this.counter === this.minNumber}">+</button>
+        <confetti-container id="confetti"></confetti-container>
+        <slot></slot>
+      </div>`;
   }
-
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
   }
 }
+
 
 globalThis.customElements.define(counterApp.tag, counterApp);
